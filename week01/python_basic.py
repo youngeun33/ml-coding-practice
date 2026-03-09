8c7be8da258f72bb978d2e26e385e372b354e0d4
김영은 <yejy020306@gmail.com>
2026-03-09 12:20:26 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -88,7 +88,7 @@ print(b.split(","))
 a =[1,2,3]
 b = ['Life', 'is', 'too', 'short']
 c = [1, 2, 'Life', 'is']
-d = [1, 2, ['Life', 'is']]
+d = [1, 2,[3,4], ['Life', 'is']]
 
 # 리스트 인덱싱
 print(d[0])
