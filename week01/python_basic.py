0b34c119f80e4226c1ef6979a0c526616c298279
김영은 <yejy020306@gmail.com>
2026-03-09 11:52:25 +0900

    Updated Changes

---
+++ week01/python_basic.py
@@ -0,0 +1,42 @@
+a = 3
+b = 4
+
+# 지수승
+print(a ** b)
+print(a**3)
+
+# 나머지 연산
+print(a % b)
+print(7 % 3)
+
+# 나눗셈 몫 구하기
+print(a // b)
+print(7 // 3)
+
+s1 = 'Hello Python'
+print(s1)
+
+s3 = """Hello 
+Python"""
+print(s3)
+
+head = "Python"
+tail = "is fun"
+print(head + tail)
+
+# 문자열 곱하기
+print(head * 2)
+print("=" * 5)
+
+#문자열 인덱싱
+a = "Now is better that never"
+print(a[0])
+print(a[4])
+print(a[-1])
+print(a[-2])
+
+#문자열 슬라이싱
+b = a[0] + a[1] +a[2]
+print(b)
+
+print(a[4:6])
