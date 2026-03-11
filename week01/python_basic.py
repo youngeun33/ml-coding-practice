0ae9193d3d28cc40b4be1eb7f2c73b5280ad88bf
김영은 <yejy020306@gmail.com>
2026-03-11 10:09:03 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -383,4 +383,11 @@ print(c)
 # 입력 받은 숫자에 해당되는 범위의 값을 반환
 print(range(5))
 print(list(range(5)))
-print(list(range(1,10,2=)))
+print(list(range(5,10)))
+print(list(range(5,10,2)))
+
+for i in range(5,20,3):
+    print(i)
+
+# 입력값 s의 길이를 반환
+le
