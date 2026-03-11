9addc354695dd303b646a1c364f8350f5ee91c8a
김영은 <yejy020306@gmail.com>
2026-03-11 09:46:33 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -224,4 +224,14 @@ print(s2)
 s3 = set([4,5,6,7,8,9])
 print(s3)
 
-# 
+# 교집합 연산
+print(s2 & s3)
+print(s2.intersection(s3))
+
+# 합집합 연산
+print(s2 | s3)
+print(s2.union(s3))
+
+# 차집합 연산
+print(s2 - s3)
+print()
