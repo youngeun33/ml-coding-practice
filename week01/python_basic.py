8c36f4fd1af5201dfd01d48ef95ff4ec296a5f26
김영은 <yejy020306@gmail.com>
2026-03-11 10:12:04 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -416,4 +416,10 @@ f.close()
 
 # 파일 쓰기 
 f = open('example.txt', 'w')
-for i i
+for i in range(1,6):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
+f.close()
+
+# 파일에 내용 추가하기
+f = open('example.txt', 'w')
