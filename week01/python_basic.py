ab6b88ed15b6efb4303cbff6f5235021489351a9
김영은 <yejy020306@gmail.com>
2026-03-11 09:43:32 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -193,5 +193,12 @@ del dic[1]
 print(dic)
 
 # 원소의 value 구하기
-print(di)
+print(dic['phone'])
 print(dic['name'])
+
+# key의 리스트 만들기
+print(dic.keys())
+print(list(dic.keys()))
+
+for ket in dic.keys():
+    print(key)
