755ae5851e36546eec1d94df07c36760cf76344d
김영은 <yejy020306@gmail.com>
2026-03-17 10:08:30 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -21,4 +21,6 @@ import pandas as pd
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample_df.csv'
 
 sample_df = pd.read_csv(file_url, index_col=0)
-print(s)
+print(sample_df.head())
+
+print(sample_df['var_5'])
