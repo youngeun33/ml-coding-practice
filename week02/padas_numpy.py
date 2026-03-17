dfb01d5b7a974023004fbe0cd3e28e1122e8cfe2
김영은 <yejy020306@gmail.com>
2026-03-17 10:10:30 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -30,3 +30,6 @@ print(sample_df[['var_1', 'var_4']])   # [ [] ]를 사용하면 [] 가 하나의
 # loc 는 location의 앞글자
 print(sample_df.loc['a'])              # 행 기준으로 인덱싱
 print(sample_df.loc[['a','c','e']])
+print(sample_df.loc['a':'c'])
+
+# iloc: integer l
