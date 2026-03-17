433059fbe5d61d2b2cf461f273cfb5b9d3553be4
김영은 <yejy020306@gmail.com>
2026-03-17 10:09:00 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -14,9 +14,8 @@ a = pd.DataFrame(sample_dic)
 
 a.info()
 
-pd.DataFrame([1,2],[3,4],[5,6],[7,8])
-pd.DataFrame([1,2],[3,4],[5,6],[7,8], columns=['val_1','var_2'], index=['a','b','c','d'])
-
+pd.DataFrame([[1,2,],[3,4],[5,6],[7,8]])
+pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns = ['var_1','var_2'], index=['a','b','c','d'])
 import pandas as pd
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample_df.csv'
 
@@ -24,3 +23,5 @@ sample_df = pd.read_csv(file_url, index_col=0)
 print(sample_df.head())
 
 print(sample_df['var_5'])
+
+# print()
