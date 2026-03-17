a82b3bb4d8fe3090a544adefd8c2a99609baee97
김영은 <yejy020306@gmail.com>
2026-03-17 10:03:00 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample.csv'
-sample = 
+sample = pd.read_csv(file_url)
+
+print(sample.head())
+print(sample.tail())
+
+sample.info()
+sample.describe()   
+
