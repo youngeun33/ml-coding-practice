ccd1f43cbde8dfb5958ee4fe0594b06d3763fdaf
김영은 <yejy020306@gmail.com>
2026-03-17 10:05:30 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -10,3 +10,9 @@ sample.info()
 sample.describe()   
 
 sample_dic = {'name':['Jonh','Ann','Kevin'], 'age' :[23,22,21]}
+a = pd.DataFrame(sample_dic)   
+
+a.info()
+
+pd.DataFrame([1,2],[3,4],[5,6],[7,8])
+pd.
