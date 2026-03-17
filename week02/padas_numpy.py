c6537d7885d745ba0c775a29c0277135ed9fc2b8
김영은 <yejy020306@gmail.com>
2026-03-17 10:39:00 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -40,4 +40,6 @@ print(sample_df.iloc[0:3, 2:4])        # 컬럼까지 동시에 인덱싱
 
 print(sample_df.drop(['var_1','var_3'], axis=1))    # 컬럼을 제거하려면 axis = 1
 print(sample_df.drop(['var_1','var_2'], axis=1))
-print(sample_df.drop(['a',''])
+print(sample_df.drop(['a','b','c'], axis=0))      # 행을 제거하려면 axis = 0 또는 디폴트로 사용 
+
+netflix = pd.read_csv('2.1.1.netflix.csv')
