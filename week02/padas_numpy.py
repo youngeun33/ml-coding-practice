aae08cee0730f740ea27f95c041f2010a7ec5b9c
김영은 <yejy020306@gmail.com>
2026-03-17 10:10:00 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -28,4 +28,5 @@ print(sample_df['var_5'])
 print(sample_df[['var_1', 'var_4']])   # [ [] ]를 사용하면 [] 가 하나의 값으로 인식됨
 
 # loc 는 location의 앞글자
-print(sample_df.loc['a']_
+print(sample_df.loc['a'])              # 행 기준으로 인덱싱
+print(sample_df.loc[['a','c','e']])
