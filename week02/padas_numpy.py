aac288c916e9ad415e4fd4973e6ec4223d5bdd6e
김영은 <yejy020306@gmail.com>
2026-03-17 10:09:30 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -24,4 +24,8 @@ print(sample_df.head())
 
 print(sample_df['var_5'])
 
-# print()
+# print(sample_df['var_1', 'var_2'])   # [ ] 안에는 하나의 값만 들어갈 수 있음
+print(sample_df[['var_1', 'var_4']])   # [ [] ]를 사용하면 [] 가 하나의 값으로 인식됨
+
+# loc 는 location의 앞글자
+print(sample_df.loc['a']_
