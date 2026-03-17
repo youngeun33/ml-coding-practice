8afcb1e444831e3d41887c9ad84a6e9e526f9ef5
김영은 <yejy020306@gmail.com>
2026-03-17 10:38:00 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -38,4 +38,5 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4])        # 컬럼까지 동시에 인덱싱 
 
-print(sample_df.drop(['var_1','v']))
+print(sample_df.drop(['var_1','var_3'], axis=1))    # 컬럼을 제거하려면 axis = 1
+print(sample_df.drop()
