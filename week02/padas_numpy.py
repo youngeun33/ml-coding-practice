45ee2a3170a377ac0b82310fe8887ba6066885bb
김영은 <yejy020306@gmail.com>
2026-03-22 09:24:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -69,6 +69,11 @@ date = {
     'name':['Alice','Bob','Charlie','David','Eve','Frank','Grace','Hannah'],
     'comment_length':[150,200,50,300,120,180,75,160],
     'likes' : [25,30,10,45,20,35,5,28],
-    'is_span' : [Fa]
-
+    'is_span' : [False, False, True, False, False, True, False, False],
+    'has_image' : [True, False, True, True, False, False, True, True]
 }
+df = pd.DataFrame(date)
+print(df.head())
+
+# 필터링 조건 설정
+condition
