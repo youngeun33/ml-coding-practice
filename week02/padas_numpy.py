d0b055b7568a0acf090149fd3d54f4612cc0a08b
김영은 <yejy020306@gmail.com>
2026-03-22 09:32:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -110,4 +110,6 @@ print(iris.drop('class', axis=1).agg(['sum','mean','std']))
 
 print(iris['class'].unique())
 print(iris['class'].nunique())
-print(iris['class'])
+print(iris['class'].value_counts())
+
+# 에제 데이터 생
