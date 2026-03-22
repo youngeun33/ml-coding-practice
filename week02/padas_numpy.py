833b6441d1c6621d85a362d5e67ef4f5626253ac
김영은 <yejy020306@gmail.com>
2026-03-22 09:28:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -93,4 +93,6 @@ print(sample_df.reset_index(drop=True))     # 기존 인덱스는 제거하기
 
 print(sample_df.set_index('var_1'))   
 
-print(sample_df.de)
+print(sample_df.describe())
+print(sample_df.std())
+print(sample_df.agg(['count','mean','std',]))
