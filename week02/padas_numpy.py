10cf75b081d01e6a60b56f9b3b33c553f29d23df
김영은 <yejy020306@gmail.com>
2026-03-22 09:51:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -203,3 +203,5 @@ print(np.random.choice(['red','green','white','black','blue'],size=3))
 print(np.random.choice(['red','green','white','black','blue'],size=3, replace=False))
 
 print(np.arange(1, 11))
+print(np.arange(1,11,2))
+print(np.linspace(1,100,10)) #
