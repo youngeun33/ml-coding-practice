98e876d549b451e75c4751ed9e6f6958b84b1e73
김영은 <yejy020306@gmail.com>
2026-03-22 09:45:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -179,4 +179,8 @@ print(pd.DataFrame(sample_np, columns=sample_df.columns))   #  새 프레임워�
 print(sample_np)
 print(sample_np[0])
 print(sample_np[0,2])
-print(sample_np)
+print(sample_np[0:3, 2:4])
+print(sample_np[:, 2:4])
+
+np_a = np.array([[1,3],[0,-2]])
+print(np_a)
