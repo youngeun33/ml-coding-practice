062a7d36cfbda8a2d893ff00735ab10d175b1ebb
김영은 <yejy020306@gmail.com>
2026-03-22 09:15:42 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -16,6 +16,8 @@ a.info()
 
 pd.DataFrame([[1,2,],[3,4],[5,6],[7,8]])
 pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns = ['var_1','var_2'], index=['a','b','c','d'])
+
+
 import pandas as pd
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample_df.csv'
 
