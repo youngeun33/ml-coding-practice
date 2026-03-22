6b3f41f8175cdabad6ac22a8cc503be86c5b5aaa
김영은 <yejy020306@gmail.com>
2026-03-22 09:38:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -139,4 +139,9 @@ data = {
 df = pd.DataFrame(data)
 print(df.head())  
 
-# 개인별 과목 점수의 평균값 계산 (axi)
+# 개인별 과목 점수의 평균값 계산 (axis=1)
+df['average'] = df[['math','science','english']].mean(axis=1)
+print(df)
+
+# 이름과 평균 값만을 포함하는 새로운 데이터프레임 생성
+average
