2dd200ea9eb02f24d30c534622b2f1db111e7c47
김영은 <yejy020306@gmail.com>
2026-03-22 09:22:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -65,4 +65,6 @@ print(more2015_tv.head())
 more2015_or_tv =  netflix[(netflix['release_year'] > 2015) | (netflix['type'] == 'TV Show')]
 print(more2015_or_tv.head()) 
 
-date = {}
+date = {
+    'name':['Alice']
+}
