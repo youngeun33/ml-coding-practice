dd0995d74e4e7793bd3a17bcc0ccf669f1299dd8
김영은 <yejy020306@gmail.com>
2026-03-22 09:26:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -79,6 +79,9 @@ print(df.head())
 condition = (
     (df['comment_length']>100)&         # 댓글 길이 100자 이상
     (df['likes']>=20)&                  # 좋아요 20개 이상 
-    (~df['is_spam'])&              # 스팸 댓글이 아니어야 함
-    (df['has_image'])
+    (~df['is_spam'])&                   # 스팸 댓글이 아니어야 함
+    (df['has_image'])                   # 이미지가 포함된 댓글이어야 함
 )
+
+# 조건을 만족하는 행들 필터링
+winner 
