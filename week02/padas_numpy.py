d3c8d462eea5d43ecad6650729f1f760eb5d26c8
김영은 <yejy020306@gmail.com>
2026-03-22 09:31:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -108,4 +108,6 @@ print(iris.drop('class', axis=1).agg(['sum','mean','std']))
 
 # print(iris.agg(['sum','mean','std']))   # class 컬럼으로 인해 에러 발생
 
-
+print(iris['class'].unique())
+print(iris['class'].nunique())
+print(iris['class'])
