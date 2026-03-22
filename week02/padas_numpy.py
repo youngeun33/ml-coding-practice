42fcba80fa11d0392cb0bf0f511c43c44c00abf9
김영은 <yejy020306@gmail.com>
2026-03-22 09:51:42 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -204,4 +204,6 @@ print(np.random.choice(['red','green','white','black','blue'],size=3, replace=Fa
 
 print(np.arange(1, 11))
 print(np.arange(1,11,2))
-print(np.linspace(1,100,10)) #
+print(np.linspace(1,100,10))       # 1부터 10까지 균등한 간격의 값을 4개추출
+
+A = np.array([4,16,25])
