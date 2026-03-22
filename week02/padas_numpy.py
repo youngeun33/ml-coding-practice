56bd2f474d5f331845bc69887bf539bff8815cc9
김영은 <yejy020306@gmail.com>
2026-03-22 09:56:12 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -216,12 +216,12 @@ a = np.arange(8).reshape(2,4)**2
 print(a)
 
 print(a.sum())    # 모든 요소의 합
-print(a.meat())    # 모든 요소의 평균
+print(a.mean())    # 모든 요소의 평균
 print(a.mean(axis=0))    # 열을 기준으로 연산
 
 print(a.min()) # 모든 요소 중 최솟값
 print(a.max()) # 모든 요소 중 최댓값
 
 print(a.max(axis=1))
-print(a.cumsum()) #모든 요소의 누적합
+print(a.cumsum()) # 모든 요소의 누적합
 print(a.argmax()) # 모든 요소 중 최댓값의 인덱스
