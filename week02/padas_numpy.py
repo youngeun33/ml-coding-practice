16bb9a2c2023a27d76ed3edb434bbe8d47d6f112
김영은 <yejy020306@gmail.com>
2026-03-22 09:37:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -131,5 +131,7 @@ print(result)
 data = {
     'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
     'math': [88,92,85,95,90],
-    'science' :[80,]  
+    'science' :[80,85,88,92,85],
+    'english' : [90,87,85,88,92]
+
 }
