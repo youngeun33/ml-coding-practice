0ee754541b95e0dd6277ea2df27f2d34cff71e6d
김영은 <yejy020306@gmail.com>
2026-03-22 09:19:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -50,11 +50,13 @@ print(netflix.head())
 print(netflix['release_year'])
 print(netflix['release_year'] > 2015)
 
-more2015 = netflix[netflix['releas_year'] > 2015]
+more2015 = netflix[netflix['release_year'] > 2015]
 print(more2015.head(10))
 
-print(~(netflix['releas_year'] > 2015))
+print(~(netflix['release_year'] > 2015))
 less2015 = netflix[~(netflix['release_year'] >2015)]
 print(less2015.head())
 
-print((netflix['release_year']))
+print((netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show'))
+
+more2015_tv = netflix[(netflix[;])]
