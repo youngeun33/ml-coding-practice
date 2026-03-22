a8135c8c0633b492b1c8ca3fdff2cdcd9910a82b
김영은 <yejy020306@gmail.com>
2026-03-22 09:44:11 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -169,4 +169,9 @@ print(np.array([[[1,2,3],
 print(np.array([1,2,3,4,5]))
 print(np.array(sample_df))
 
-sample_np = np.
+sample_np = np.array(sample_df)
+print(pd.DataFrame(sample_np))      # 컬럼명이 0, 1, 2, 3, 4
+
+print(sample_df.columns)            # 기존 프레임워크에서 컬럼명 가져오기
+
+print(pd)
