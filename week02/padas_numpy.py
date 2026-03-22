8a1a1f97dd70a776020ac0fae2900930a9867dcc
김영은 <yejy020306@gmail.com>
2026-03-22 09:40:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -153,4 +153,7 @@ import numpy as np
 print(np.array([1,2,3]))
 
 print(np.array ([[1,2,3],
-                 [4,5,6]]
+                 [4,5,6],
+                 [7,8,9]]))
+
+print(np.array([[1,2,3],]))
