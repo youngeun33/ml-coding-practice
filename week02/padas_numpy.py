191e971a7e60e7d2a85b732c35ed9847621b2548
김영은 <yejy020306@gmail.com>
2026-03-22 09:44:41 +0900

    Updated Changes

--- week02/padas_numpy.py
+++ week02/padas_numpy.py
@@ -174,4 +174,4 @@ print(pd.DataFrame(sample_np))      # 컬럼명이 0, 1, 2, 3, 4
 
 print(sample_df.columns)            # 기존 프레임워크에서 컬럼명 가져오기
 
-print(pd)
+print(pd.DataFrame(sample_np, columns=sample_df.columns))   #  새 프레임워크에 컬럼명 붙이기
