d52fffdb3d7b10558b414d11e84b0cb4401eab60
김영은 <yejy020306@gmail.com>
2026-03-26 21:28:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -160,4 +160,7 @@ plt.close()
 titanic = titanic.dropna(subset=['Age','Fare'])
 
 # 나이대별 생존자와 사망자 수 계산하기 위해 범주형 변수로 전환
-age_groups = pd.cut(titanic.)
+age_groups = pd.cut(titanic['Age'], bins=range(0,81,5))
+
+# Age, Survived 기준으로 그룹화
+
