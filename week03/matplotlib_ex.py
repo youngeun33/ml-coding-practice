7c1e672b76789692282223390d35b0dbbc94e476
김영은 <yejy020306@gmail.com>
2026-03-26 20:43:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -128,4 +128,5 @@ plt.figure(figsize=(10,6))
 plt.hist(titanic['Age'],bins=20,color='seagreen',edgecolor='balck')
 
 plt.xlabel('Age')
-plt
+plt.ylabel('Count')
+plt.title('Distribution of Ages on the Tit')
