564f91ca8fc3310f759f311b78f365946b464111
김영은 <yejy020306@gmail.com>
2026-03-26 20:18:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -37,4 +37,8 @@ plt.close()                         # 다음 Plot을 새로 그리기 위해 plt
 
 # 승선 항구에 따른 생존자의 수 계산
 survived_counts = titanic[titanic['Survived']==1]['Embarked'].value_counts()
-print(survived_conuts)
+print(survived_counts)
+
+# 막대 그래프 그리기
+plt.bar(survived_counts.index, survived_counts,
+        )
