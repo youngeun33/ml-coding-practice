312cf30f3113cb7b7c69e823bea6580090722da7
김영은 <yejy020306@gmail.com>
2026-03-26 20:26:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -64,4 +64,6 @@ print(survived_counts)
 
 # 수평 막대 그래프 그리기
 bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise','salmon'])
-plt.title('Sur')
+plt.title('Survived Counts by Gender on Titanic')
+plt.xlabel('Count')
+plt.ylabel(Gender)
