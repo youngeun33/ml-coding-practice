f08244f9a86533b63ed536a2b71c2ac0cfdc6f73
김영은 <yejy020306@gmail.com>
2026-03-26 20:32:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -88,4 +88,5 @@ titanic = titanic.dropna(subset=['Age','Fare','Survived'])
 print(titanic.info())
 
 # 산점도 그래프 그리기 
-plt.figure(figsize=)
+plt.figure(figsize=(12,8))
+scatter = plt.scatter(x='Age',y='Fare',data=titanic,)
