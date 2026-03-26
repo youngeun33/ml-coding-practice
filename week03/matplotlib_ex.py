c42176d8c3ba23e574290cae040721a246411412
김영은 <yejy020306@gmail.com>
2026-03-26 20:29:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -74,4 +74,8 @@ plt.axvline(x=survived_counts['male'],color='gray',linestyle='--',linewidth=1)
 
 # 생존자 수 표시
 for i,value in enumerate(survived_counts):
-    plt.text(value+1, i , str(value),ha='left',va=)
+    plt.text(value+1, i , str(value),ha='left',va='center')
+
+plt.savefig('Figure03.png')
+plt.close()
+
