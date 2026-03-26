a0389bd4ff6a8aa93ecba58700523e3d704ebb25
김영은 <yejy020306@gmail.com>
2026-03-26 20:39:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -107,4 +107,5 @@ print(survived_counts)
 
 # 파이 차트 그리기
 plt.figure(figsize=(8,8))
-plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['ora'])
+plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange','gold'],
+        autopct='%0.1f%%',startangle=90,sha)
