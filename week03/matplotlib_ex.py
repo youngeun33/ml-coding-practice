579ad04f8798ce7a50df91c3bcd531ffdfefd7bc
김영은 <yejy020306@gmail.com>
2026-03-26 21:41:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -209,12 +209,14 @@ plt.close()
 
 # 결측치 처리
 titanic['Age'] = titanic['Age'].fillna(titanic['Age'].mean())
-
+print(titanic.info(*))
 # 바이올린 플롯 그리기
 plt.figure(figsize=(10,6))
 
 # showmeans=False는 평균값을 표시하지 않도록 하고, showmedians=True는 중앙값을 표시하도록 함
 violin_plot = plt.violinplot([titanic[titanic['Pclass']==1]['Age'],
-                              titanic[titanic['Pclass']==1]['Age'],
-                              titanic[titanic['Pclass']==1]['Age']],
-                              showmeans=False,Sh)
+                              titanic[titanic['Pclass']==2]['Age'],
+                              titanic[titanic['Pclass']==3]['Age']],
+                              showmeans=False,showmedians=True)
+
+plt.
