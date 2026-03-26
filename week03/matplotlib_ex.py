6712dada3497438b94258ee5f22f72787591769a
김영은 <yejy020306@gmail.com>
2026-03-26 21:35:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -178,4 +178,8 @@ plt.fill_between(survived_counts.index.astype(str),survived_counts[1],
 plt.fill_between(survived_counts.index.astype(str),survived_counts[0],
                  color='hotpink',alpha=0.6,label='Not Survived')
 
-plt.title('Survival by Age Gr')
+plt.title('Survival by Age Group on Titanic')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend()
