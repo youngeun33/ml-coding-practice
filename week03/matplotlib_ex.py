adbfdac087290421d52622f030395f9275447fc3
김영은 <yejy020306@gmail.com>
2026-03-26 20:18:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -36,4 +36,5 @@ plt.close()                         # 다음 Plot을 새로 그리기 위해 plt
 """### **수직 막대 그래프 : 각 승선 항구에 따른 생존자 수 확인하기**"""
 
 # 승선 항구에 따른 생존자의 수 계산
-survived_conts = titanic[titanic['Survived']==1[['Embarked'].]
+survived_counts = titanic[titanic['Survived']==1]['Embarked'].value_counts()
+print(survived_conuts)
