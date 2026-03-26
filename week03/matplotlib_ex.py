1dda630dd16472028a1ab5792703ac0ad8186166
김영은 <yejy020306@gmail.com>
2026-03-26 20:24:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -57,3 +57,6 @@ plt.savefig('Figure02.png')
 plt.close()
 
 """### **수평 막대 그래프 : 성별에 따른 생존자 수 확인하기**"""
+
+# tjdqufdp Ekfms todwhswkdml tn rPtks
+survived_counts = titanic[titanic['Survived']==1]['Sex'].value_counts()
