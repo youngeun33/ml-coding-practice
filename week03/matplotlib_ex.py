6317fc8362a0a116afb142bca3a8a6ab8d34ad63
김영은 <yejy020306@gmail.com>
2026-03-26 20:16:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -31,4 +31,6 @@ plt.ylabel('Survival Rate')
 plt.xticks([1,2,3])
 plt.grid(True)
 plt.savefig('Figure01.png')         # 결과를 그림파일로 저장
-plt.close() 
+plt.close()                         # 다음 Plot을 새로 그리기 위해 plt 닫기
+
+"""### **수직 막대 그래프 : 각 승선 항구에 따른 생존자 수 확인하기**"""
