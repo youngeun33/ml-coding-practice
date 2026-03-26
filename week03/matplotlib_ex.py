bbb8dfe48e168bc1e88272561a5b5b39af6b8279
김영은 <yejy020306@gmail.com>
2026-03-26 20:40:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -108,4 +108,6 @@ print(survived_counts)
 # 파이 차트 그리기
 plt.figure(figsize=(8,8))
 plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange','gold'],
-        autopct='%0.1f%%',startangle=90,sha)
+        autopct='%0.1f%%',startangle=90,shadow=True,explode=(0,0.1))
+
+plt.title('Survival Distribution on the Ti')
