204cb1e90014fc981d74cb9157752deaef76b145
김영은 <yejy020306@gmail.com>
2026-03-26 20:23:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -48,3 +48,7 @@ plt.ylabel('Count')
 plt.xticks(survived_counts.index,['Southampton','Cherbourg','Queenstown'])
 plt.legend(['Survived'], loc='upper right')
 plt.grid(axis='y',linestyle='--',alpha=0.7)
+
+# 생존자 수 표시
+for i, value in enumerate(survived_counts):
+    plt.text()
