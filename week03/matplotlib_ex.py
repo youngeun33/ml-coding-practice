70fd7986f1f1cbe328d754f8b823de61a17ce76c
김영은 <yejy020306@gmail.com>
2026-03-26 21:40:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -213,4 +213,5 @@ titanic['Age'] = titanic['Age'].fillna(titanic['Age'].mean())
 # 바이올린 플롯 그리기
 plt.figure(figsize=(10,6))
 
-# showmeans=False는 평균값을 표시하지 않도록 하고, sh
+# showmeans=False는 평균값을 표시하지 않도록 하고, showmedians=True는 중앙값을 표시하도록 함
+violin_plot = plt.violinplot([titanic[titanic['Pclass']==1]['Age']])
