0bf72a5d0f521a61075a253c58f40b10a01453f1
김영은 <yejy020306@gmail.com>
2026-03-26 20:38:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -99,4 +99,8 @@ plt.legend(handles=scatter.legend_elements()[0],title='Survived',
 plt.savefig('Figure04.png')
 plt.close()
 
-"""### **파이 차트 : 생존자, 사망"""
+"""### **파이 차트 : 생존자, 사망자 비율 표현하기**"""
+
+# 사망자와 생존자의 수 계산
+survived_counts = titanic['Survived'].value_counts()
+print(survived_counts)
