56fddec65ac9b570ee5452b817d77991cfa5518a
김영은 <yejy020306@gmail.com>
2026-03-26 21:27:29 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -153,3 +153,8 @@ plt.yticks(range(len(correlation_matrix.columns)),correlation_matrix.columns)
 plt.title('Correlation Heatmap of Titanic')
 plt.savefig('Figure07.png')
 plt.close()
+
+## **영역 채우기 그래프 : 나이대별 생존자와 사망자 수 표현하기**
+
+# 결측치 처리
+ti
