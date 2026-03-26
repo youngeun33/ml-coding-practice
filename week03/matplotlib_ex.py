9517510464358af6d7b50fc14701031c9225543b
김영은 <yejy020306@gmail.com>
2026-03-26 20:34:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -96,4 +96,5 @@ plt.xlabel('Age')
 plt.ylabel('Fare')
 plt.legend(handles=scatter.legend_elements()[0],title='Survived',
            labels=['Not Survived','Survived'],loc='upper right')
-plt.savefig(;)
+plt.savefig('Figure04.png')
+plt.close()
