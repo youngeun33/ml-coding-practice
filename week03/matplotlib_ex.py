36ce802663fce46068d0beb6a1c2707153310612
김영은 <yejy020306@gmail.com>
2026-03-26 20:33:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -91,4 +91,6 @@ print(titanic.info())
 plt.figure(figsize=(12,8))
 scatter = plt.scatter(x='Age',y='Fare',data=titanic,c=titanic['Survived'],cmap='Set2',alpha=0.7)
 
-plt.title('Age and Relationship[]')
+plt.title('Age and Relationship with Survived on the Titanic')
+plt.xlabel('Age')
+plt.ylabel('Fare')
