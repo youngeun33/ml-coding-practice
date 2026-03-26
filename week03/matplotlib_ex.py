d49440bb07ecb7334e9924a73742aabfa81dbfe9
김영은 <yejy020306@gmail.com>
2026-03-26 20:25:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -63,4 +63,5 @@ survived_counts = titanic[titanic['Survived']==1]['Sex'].value_counts()
 print(survived_counts)
 
 # 수평 막대 그래프 그리기
-bars = plt.barh(survived_counts.index, survived_counts, color = )
+bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise','salmon'])
+plt.title('Sur')
