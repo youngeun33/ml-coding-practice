09c179258dc22ab9bb27a423d1bc527322dfceb8
김영은 <yejy020306@gmail.com>
2026-03-26 20:01:05 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -5,4 +5,7 @@ import pandas as pd
 # 타이타닉 csv 파일 불러오기
 titanic = pd.read_csv('3.1.1.tatanic.csv')
 
-# head( ) 함수를 출력하여
+# head( ) 함수를 출력하여 타이타닉 데이터셋의 구성을 간단히 살펴보기
+
+# 데이터 처음 5개의 행 출력
+print(titanic.head())
