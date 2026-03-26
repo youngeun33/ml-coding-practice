6f6ac98ca1de8749258bfe1e7f253333a70de10b
김영은 <yejy020306@gmail.com>
2026-03-26 20:45:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -125,7 +125,7 @@ print(titanic.info())
 
 # 히스토그램 그리기
 plt.figure(figsize=(10,6))
-plt.hist(titanic['Age'],bins=20,color='seagreen',edgecolor='balck')
+plt.hist(titanic['Age'],bins=20,color='seagreen',edgecolor='black')
 
 plt.xlabel('Age')
 plt.ylabel('Count')
