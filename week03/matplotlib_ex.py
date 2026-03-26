0cb960efc92d51d60423d162748fb1af23a0394f
김영은 <yejy020306@gmail.com>
2026-03-26 21:26:29 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -147,4 +147,7 @@ plt.matshow(correlation_matrix, cmap='PuRd_r')
 plt.colorbar()
 
 # x축과 y축의 눈금 설정
-plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotati)
+plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=45)
+plt.yticks(range(len(correlation_matrix.columns)),correlation_matrix.columns)
+
+plt.title()
