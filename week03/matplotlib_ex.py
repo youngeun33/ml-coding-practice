afb08ceee117d1832c7c19cda3aac73b20166827
김영은 <yejy020306@gmail.com>
2026-03-26 21:34:29 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -173,3 +173,6 @@ plt.figure(figsize=(10,6))
 # 나이대별 생존자
 plt.fill_between(survived_counts.index.astype(str),survived_counts[1],
                  color = 'purple',alpha=0.9,label='Not Survived')
+
+# 나이대별 사망자
+plt.fill_between(survived_counts.index.as)
