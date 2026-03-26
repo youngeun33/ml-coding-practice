26547c52376b3bedfe72721f326f9a703b5ae8ca
김영은 <yejy020306@gmail.com>
2026-03-26 20:24:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -54,4 +54,6 @@ for i, value in enumerate(survived_counts):
     plt.text(i,value+1,str(value),ha='center',va='bottom')
 
 plt.savefig('Figure02.png')
-plt.cl
+plt.close()
+
+"""### **수평 막대 그래프 : 성별에 따른 생존자 수 확인하기**"""
