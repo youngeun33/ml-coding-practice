32943555abea4d87b31f51823d936e7752f7bcf6
김영은 <yejy020306@gmail.com>
2026-03-26 21:36:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -187,4 +187,8 @@ plt.grid(True)
 plt.savefig('Figure08.png')
 plt.close()
 
-## **박스 플롯 : 승객 나이의 데이터 분포, 
+## **박스 플롯 : 승객 나이의 데이터 분포, 중앙값, 이상치 살펴보기**
+
+# 결측치 처리
+titanic = titanic.dropna(subset=['Age'])
+print(titanic.info())
