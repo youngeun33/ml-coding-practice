0c91f6390f3039622fb4a1253586b3975a847f08
김영은 <yejy020306@gmail.com>
2026-03-26 20:40:58 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -110,4 +110,8 @@ plt.figure(figsize=(8,8))
 plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange','gold'],
         autopct='%0.1f%%',startangle=90,shadow=True,explode=(0,0.1))
 
-plt.title('Survival Distribution on the Ti')
+plt.title('Survival Distribution on the Titanic')
+plt.savefig('Figure05.png')
+plt.close()
+
+""
