b259a426827eae8878404cb8522968c241f9e1bb
김영은 <yejy020306@gmail.com>
2026-03-28 12:11:19 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -284,4 +284,8 @@ print(parch_counts)
 # x, y 설정
 x = parch_counts.index.astype(str)  # 데이터프레임의 인덱스를 문자열로 반환
 y1 = parch_counts[0].values # 사망자
-y2 = parch_counts[1].values
+y2 = parch_counts[1].values # 생존자
+
+plt.figure(figsize=(10,10))
+
+# plt.subplot() 함수를 사용하여 하나의 그림인 figure에 개별 서브 프
