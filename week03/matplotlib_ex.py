061a0edafd8e1dbaa5698637188c2a12d77b1a70
김영은 <yejy020306@gmail.com>
2026-03-28 12:23:49 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -334,4 +334,6 @@ plt.close()
 parch_counts = titanic.groupby('Parch')['Survived'].value_counts().unstack().fillna(0)
 print(parch_counts)
 
-# ㅌ
+# x, y값 결정
+x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 변환
+ㅛ1
