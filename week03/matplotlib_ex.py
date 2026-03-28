d7e96c676334a7b5f72c41b5db34126ae450b896
김영은 <yejy020306@gmail.com>
2026-03-28 12:02:19 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -236,4 +236,6 @@ plt.close()
 ## **에러 바 : 요금의 평균과 표준편차 표현하기**
 
 # 각 부모와 자녀의 수에 대한 요금의 평균과 표준편차 계산
-fare_means = titanic.groupby('Parc')
+fare_means = titanic.groupby('Parch')['Fare'].mean()  # 평균
+
+fare_std = titanic.groupby('Parch')['Fare'].std()   # ㅍ
