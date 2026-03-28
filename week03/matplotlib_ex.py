40dd1cbabefa7e9d33dc75c30c73be85488e1491
김영은 <yejy020306@gmail.com>
2026-03-28 12:30:19 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -340,7 +340,7 @@ y1 = parch_counts[0].values # 사망자
 y2 = parch_counts[1].values # 생존자
 
 # plt.subplots() 함소를 사용하여 하나의 그림인 figure 개별 서브플롯을 동시에 추가
-fig, axes = plt.subplot(2,1,figsize=(10,10))
+fig, axes = plt.subplots(2, 1, figsize=(10, 10))
 
 # 첫 번째 서브플롯 설정 (선 그래프)
 axes[0].plot(x,y1,'-o',color='indigo',markersize=7,linewidth=3,alpha=0.7,label='Not Survived')
@@ -357,4 +357,5 @@ axes[1].tick_params(axis='y',labelcolor='deeppink')
 axes[1].legend(loc='upper right')
 
 # 제목 설정
-fig.suptitle()
+fig.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+fig.tight_layout
