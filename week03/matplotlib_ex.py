2f7afd6b2a1c191d84d1dfb7664e90a73039e6ba
김영은 <yejy020306@gmail.com>
2026-03-28 12:32:19 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -361,3 +361,7 @@ fig.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Tit
 fig.tight_layout()
 plt.savefig('Figure15.png')
 plt.close()
+
+## **하나의 서브플롯에 여러 그래프 그리기**
+
+# 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
