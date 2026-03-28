4532df4ae12ba92755485660e9cf1fc053abf335
김영은 <yejy020306@gmail.com>
2026-03-28 12:31:19 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -350,7 +350,7 @@ axes[0].tick_params(axis='y',labelcolor='indigo')
 axes[0].legend(loc='upper right')
 
 # 두 번째 서브플롯 설정(막대 그래프)
-axes[1].bar(x,y2,'-o',color='indigo',alpha=0.7,width=0.5,label='Survived')
+axes[1].bar(x, y2, color='deeppink', alpha=0.7, width=0.5, label='Survived')
 axes[1].set_xlabel('Parch')
 axes[1].set_ylabel('Survived Count',color='deeppink')
 axes[1].tick_params(axis='y',labelcolor='deeppink')
