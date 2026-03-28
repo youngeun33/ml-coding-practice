9c722edb816c5dae9f3b4a1d2e78720bf82d64e6
김영은 <yejy020306@gmail.com>
2026-03-28 12:06:19 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -253,4 +253,8 @@ plt.title('Error Bar Plot of Fare by Parch')
 plt.xlabel('Parch')
 plt.ylabel('Fare')
 plt.xticks(fare_means.index)
-plt.
+plt.legend()
+plt.savefig('Figure11.png')
+plt.close()
+
+## **개별 서브플롯을 하나씩 생성하기**
