75d62137e47221df545697698e832a69c4e2a5c8
김영은 <yejy020306@gmail.com>
2026-03-28 12:12:49 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -292,4 +292,7 @@ plt.figure(figsize=(10,10))
 # 첫 번째 서브플롯 설정 (선그래프)
 plt.subplot(2,1,1)
 # 선 그래프 그리기
-plt.plto(x, y1, '-o', color = 'indigo',markersize=7,linewidth=3,ap)
+plt.plto(x, y1, '-o', color = 'indigo',markersize=7,linewidth=3,alpha=0.7,
+         label='Not Survived')
+plt.xlabel('Parch')
+plt.ylabe()
