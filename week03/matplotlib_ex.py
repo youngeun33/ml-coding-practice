b2d634e06b95c155fb97ec01ddace3b50b72f1b1
김영은 <yejy020306@gmail.com>
2026-03-28 12:38:50 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -397,3 +397,8 @@ plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Tit
 plt.tight_layout()
 plt.savefig('Figure16.png')
 plt.close()
+
+## **그래프 저장하기**
+
+# 히스토그램 그리기
+plt
