3389da0dcd1570b3333ea89fed9198442004dd4a
김영은 <yejy020306@gmail.com>
2026-03-28 12:03:49 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -246,4 +246,6 @@ fare_std = titanic.groupby('Parch')['Fare'].std()   # 표준 편차
 plt.figure(figsize=(10,6))
 
 # 에러바 생성
-plt.errorbar(fare_means.)
+plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='0',
+             capsize=5,capthick=1,label='Fare')
+
