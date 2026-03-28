c9d5a3770c1f33e01ab46ea4409cfe8db266cb36
김영은 <yejy020306@gmail.com>
2026-03-28 12:26:49 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -344,3 +344,5 @@ fig, axes = plt.subplot(2,1,figsize=(10,10))
 
 # 첫 번째 서브플롯 설정 (선 그래프)
 axes[0].plot(x,y1,'-o',color='indigo',markersize=7,linewidt=3,alpha=0.7,label='Not Survived')
+axes[0].set_xlabel('Parch')
+axes[0].set_ylabel('Not Survived')
