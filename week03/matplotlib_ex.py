826667c8d42241dc6874426d7bcba487ed524f97
김영은 <yejy020306@gmail.com>
2026-03-28 12:38:20 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -394,3 +394,6 @@ ax2.tick_params(axis='y',labelcolor='deeppink')
 ax2.legend(loc='upper right', bbox_to_anchor=(1,0.9))
 
 plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+plt.tight_layout()
+plt.savefig('Figure16.png')
+plt.close()
