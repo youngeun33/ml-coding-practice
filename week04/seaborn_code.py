d44b72232ebe59672dcc7060c701c3e2c662516d
김영은 <yejy020306@gmail.com>
2026-04-03 12:47:44 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -53,4 +53,5 @@ fig = plt.figure(figsize=(15,5))
 ax1 = fig.add_subplot(1,2,1)
 ax2 = fig.add_subplot(1,2,2)
 
-# 산점도에 선형 회귀선 표시(fit)
+# 산점도에 선형 회귀선 표시(fit_reg = True)
+sns.regplot(x='total_bill',y='tip',data=tips,color='blue',scatter_kws={'s':50,'alpha':0.5},line_kws={'linestyle':'--'},ax=ax1)
