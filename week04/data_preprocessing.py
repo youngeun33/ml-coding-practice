24ea2d8f9862781aa060e027061264d309dacf03
김영은 <yejy020306@gmail.com>
2026-04-03 09:11:11 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -45,3 +45,10 @@ imputer.fit(housing_num)
 
 print(imputer.statistics_)          # imputer 결과 값  
 print(housing_num.median().values)  # 수동으로 계산한 중간값
+
+# 훈련 세트의 누락값을 iumputer가 학습한 값으로 채우기
+X = imputer.transform(housing_num)
+
+imputer.feature_names_in_
+
+housing_tr = pd.DataFrame(X, col)
