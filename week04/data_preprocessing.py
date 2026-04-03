763c27efc2521b4f9ed8da90f95548ccc459c79e
김영은 <yejy020306@gmail.com>
2026-04-03 09:15:11 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -75,4 +75,6 @@ from sklearn.preprocessing import OrdinalEncoder
 ordinal_encoder = OrdinalEncoder()
 housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
 
-housing_cat
+housing_cat_encoded[:8]
+
+ordinal_encoder.categories_
