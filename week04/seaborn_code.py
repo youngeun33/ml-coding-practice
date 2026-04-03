2db6cb37b0329dc24c4e061c471b88d5de2552d7
김영은 <yejy020306@gmail.com>
2026-04-03 12:59:45 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -75,3 +75,7 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 # jointplot( ) 그리기
 sns.jointplot(x='size',y='tip',data=tips,kind='scatter')
 plt.savefig('./week04/Seaborn_Figure05.jpg')
+
+# **관계 그래프**
+# pairplot( ) 그리기
+sns.pairplot(data=tips, hue='ses',)
