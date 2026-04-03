0bfc6e071f12a66289cc5b75db2da6277df9acb1
김영은 <yejy020306@gmail.com>
2026-04-03 13:01:15 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -78,7 +78,7 @@ plt.savefig('./week04/Seaborn_Figure05.jpg')
 
 # **관계 그래프**
 # pairplot( ) 그리기
-sns.pairplot(data=tips, hue='sex',diag_kind='his', palette='hus1')
+sns.pairplot(data=tips, hue='sex', diag_kind='hist', palette='husl')
 
-plt.suptitle('Pair Plot of Tips Dataset', y=1.05)
+plt.suptitle('Pairplot with Histograms by Gender', y=1.05)
 plt.savefig('./week04/Seaborn_Figure06.jpg')
