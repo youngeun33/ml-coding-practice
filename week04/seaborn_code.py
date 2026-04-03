800c7d30fe8daa3dcdaab58f4ab3643c36c2e170
김영은 <yejy020306@gmail.com>
2026-04-03 12:58:45 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -67,4 +67,6 @@ plt.savefig('./week04/Seaborn_Figure03.jpg')
 # **히스토그램과 커널 밀도 추정 그래프**
 # 히스토그램과 커널 밀도 추정 그래프 함께 그리기
 sns.histplot(tips['tip'],bins=20,kde=True,color='skyblue')
-plt.savefig('./week04/Se
+
+plt.title('Histogram and KDE of Tips')
+plt.save)
