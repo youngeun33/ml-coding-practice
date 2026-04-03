d422395ee483212ea1e03c096b9cb224f1975eae
김영은 <yejy020306@gmail.com>
2026-04-03 09:12:41 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -56,4 +56,7 @@ housing_tr = pd.DataFrame(X, columns=housing_num.columns,
 housing_tr.loc[null_rows_idx].head()
 
 # 이상치 삭제
-from sklearn.ensemble import
+from sklearn.ensemble import IsolationForest
+
+isolation_forest = IsolationForest(random_state=42)
+outlier_pred = 
