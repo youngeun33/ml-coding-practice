35ace68e346a266f5dcc65e27a67a3c8e94d390f
김영은 <yejy020306@gmail.com>
2026-04-03 09:09:41 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -38,4 +38,9 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(strategy="median")
 
 # 수치형 특성만 추출
-housing_num = housing.selcet_dtypes(include=)
+housing_num = housing.select_dtypes(include=[np.number])
+housing_num.head()
+
+imputer.fit(housing_num)
+
+print(imputer)
