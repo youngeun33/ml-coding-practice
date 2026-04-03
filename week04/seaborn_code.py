6ca12318aea68d0925abcae0711361e145388917
김영은 <yejy020306@gmail.com>
2026-04-03 12:48:45 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -59,4 +59,4 @@ sns.regplot(x='total_bill',y='tip',data=tips,color='blue',scatter_kws={'s':50,'a
 # 산점도에 선형 회귀선 미표시(fit_reg = False)
 sns.regplot(x='total_bill',y='tip',data=tips,color='blue',scatter_kws={'s':50,'alpha':0.5},line_kws={'linestyle':'--'},ax=ax2,fit_reg=False)
 
-fig.
+fig.suptitle('Scatter Plots of Re')
