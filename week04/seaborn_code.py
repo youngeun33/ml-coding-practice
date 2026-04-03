93b233bb069ad9807055e2bcf9113354bf416689
김영은 <yejy020306@gmail.com>
2026-04-03 12:47:15 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -50,4 +50,7 @@ plt.savefig('./week04/Seaborn_Figure02.jpg')
 # **선형 회귀선 있는 산점도**
 # figure에 2개의 서브 플롯을 생성
 fig = plt.figure(figsize=(15,5))
-ax1 = fig.add
+ax1 = fig.add_subplot(1,2,1)
+ax2 = fig.add_subplot(1,2,2)
+
+# 산점도에 선형 회귀선 표시(fit)
