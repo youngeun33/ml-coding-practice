f1d68803cb114ccc17008cc76ceed31f1f8ebd2e
김영은 <yejy020306@gmail.com>
2026-04-03 12:46:45 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -47,4 +47,7 @@ ax1.set_title('Frequency of Tips by Time')
 ax2.set_title('Frequency of Tips by Time and Day ')
 plt.savefig('./week04/Seaborn_Figure02.jpg')
 
-# **선
+# **선형 회귀선 있는 산점도**
+# figure에 2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15,5))
+ax1 = fig.add
