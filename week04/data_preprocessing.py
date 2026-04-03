3ecbd56f3430bb9ad39c192ad00421f648c6a85f
김영은 <yejy020306@gmail.com>
2026-04-03 09:13:11 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -59,4 +59,8 @@ housing_tr.loc[null_rows_idx].head()
 from sklearn.ensemble import IsolationForest
 
 isolation_forest = IsolationForest(random_state=42)
-outlier_pred = 
+outlier_pred = isolation_forest.fit_predict(X)
+
+outlier_pred
+
+housing = housing.iloc[outlier_pred]
