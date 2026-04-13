73e644d7e1c4a1f528ae28efcca1500e953da1c5
김영은 <yejy020306@gmail.com>
2026-04-14 05:30:28 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -3,20 +3,20 @@ import urllib.request
 import datetime
 import json
 
-client_id = 'Client_ID'
-client_secret = 'Client_Secret'
+client_id = 'Client ID'
+client_secret = 'Client Secret'
 
 def main():
 
-    node = 'news'
-    srcText = input('검색어를 입력하세요: ')        # 크롤링할 대상
+    node = 'news'                                             # 크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
 
     cnt = 0
     jsonResult = []
 
     jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
-    total = jsonResponse['total']  
-    
+    total = jsonResponse['total']
+
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
@@ -24,49 +24,51 @@ def main():
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
-    
+
     print('전체 검색 : %d 건' %total)
 
-    with open('%s_namver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
-        jsonFile = json.dumps(jsonResult, indent=4, sort_keys=True,
-                              ensure_ascii=False)
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
+                              ensure_ascii = False)
 
         outfile.write(jsonFile)
-    
-    print("가져온 데이터 : %d 건"%(cnt))
+
+    print("가져온 데이터 : %d 건" %(cnt))
     print('%s_naver_%s.json SAVED' % (srcText, node))
+
 
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
-    parameters = "?query=%sstart=%s&display=%s" %(urllib.parse.quote(srcText), page_start, display)
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
 
     url = base + node + parameters
-    responseDecode = getNaverSearch(url)              #[CODE 1]
+    responseDecode = getRequestUrl(url)               #[CODE 1]
 
     if (responseDecode == None):
         return None
     else:
         return json.loads(responseDecode)
-    
+
 
 def getRequestUrl(url):
     req = urllib.request.Request(url)
 
-    req.add_header("X-naver-Client_Id", client_id)
-    req.add_header("X-naver-Client-Secret", client_secret)
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
 
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            print("[%s] url Request Success" % datetime.datetime.now())
+            print("[%s] Url Request Success" % datetime.datetime.now())
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
         print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
         return None
 
-def getPostData(post, jsonResult, cnt):  # [CODE 3]
+
+def getPostData(post, jsonResult, cnt):  #[CODE 3]
     title = post['title']
     description = post['description']
     org_link = post['originallink']
@@ -74,11 +76,11 @@ def getPostData(post, jsonResult, cnt):  # [CODE 3]
 
     # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
     # %d: 일 (예, 01, 02, 03, ...)
-    # %b: 짧은 형식의 월 이름 (예, 'Jan', 'Feb', 'Mar', ... )
+    # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...)
     pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
-    PDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
 
-    jsonResult.appen({
+    jsonResult.append({
         'cnt': cnt,
         'title': title,
         'description': description,
