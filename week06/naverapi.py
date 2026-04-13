52f775f9120b5673ee4372e76edad145f5478559
김영은 <yejy020306@gmail.com>
2026-04-14 05:29:58 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -16,6 +16,7 @@ def main():
 
     jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
     total = jsonResponse['total']  
+    
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
