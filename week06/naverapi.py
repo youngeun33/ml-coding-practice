e986d1c54a75c73e9f6b89db852eb3c3d283e8d6
김영은 <yejy020306@gmail.com>
2026-04-14 05:27:28 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -72,4 +72,6 @@ def getPostData(post, jsonResult, cnt):  # [CODE 3]
     link = post['link']
 
     # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
-    # %d: 일 (ㅇ)
+    # %d: 일 (예, 01, 02, 03, ...)
+    # %b: 짧은 형식의 월 이름 (예, 'Jan', 'Feb', 'Mar', ... )
+    pDate = date
