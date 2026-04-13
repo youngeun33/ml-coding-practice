bd8b33d25240dba12790656a99b0383d2acfe691
김영은 <yejy020306@gmail.com>
2026-04-14 05:36:58 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -19,3 +19,7 @@ def main():
     ed_cd = "E"                               # E : 방한외래관광객, D : 해외 출국
 
     jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
+
+    #파일저장 : csv 파일
+    columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
+    resutl_df = pd.DateaFrame(result, c)
