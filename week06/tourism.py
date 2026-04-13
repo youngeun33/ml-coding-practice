376434990010776743aa068e7f501fd195581842
김영은 <yejy020306@gmail.com>
2026-04-14 05:34:58 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,5 +4,12 @@ import datetime
 import json
 import pandas as pd
 
-SeviceKey = "200281100206dd4ac6cdacf7d0e3a9061cf1502ccd847851812036590082c162"
+ServiceKey = "200281100206dd4ac6cdacf7d0e3a9061cf1502ccd847851812036590082c162"
 
+"""### [CODE 0]"""
+
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 국내 입국한 외")
