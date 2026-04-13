5ee6a762e72187c8b8c310e2ff170c09e8defaad
김영은 <yejy020306@gmail.com>
2026-04-14 05:20:27 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -26,4 +26,10 @@ def main():
     
     print('전체 검색 : %d 건' %total)
 
-    with open('%s_namver_%s.json' % (srcText, node), 'w')
+    with open('%s_namver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent=4, sort_keys=True,
+                              ensure_ascii=False)
+
+        outfile.write(jsonFile)
+    
+    print("가져온 데이터 : %d 건"%(cnt))
