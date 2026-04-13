7595387d07dee7a7bb9be6db4f7d8587283345f7
김영은 <yejy020306@gmail.com>
2026-04-14 05:25:28 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -59,4 +59,8 @@ def getRequestUrl(url):
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
             print("[%s] url Request Success" % datetime.datetime.now())
-            return response.read()
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
