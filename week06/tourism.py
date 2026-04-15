2b7d2395e12f0608946c7f22c3cc570045333994
김영은 <yejy020306@gmail.com>
2026-04-15 09:20:24 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -27,4 +27,30 @@ def main():
 
 """### [CODE 3]"""
 
-def getTourimsStatusService(not_cd, ed_cd, )
+def getTourimsStatusService(not_cd, ed_cd, nStartYear, nEndYear):
+    jsonResult = []
+    result = []
+
+    for year in range(nStartYear, nEndYear+1):
+        for month in range(1, 13):
+            yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)     #[CODE 2]
+            if (jsonData['response']['header']['resultMsg'] == 'OK'):
+                #데이터가 없는 마지막 항목인 경우 ----------------------------
+                if jsonData['response']['body']['items'] == '':
+                    dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
+                    print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
+                    break
+                #jsonData를 출력하여 확인............................................
+                print(json.dumps(jsonData, indent = 4, sort_keys = True, ensure_ascii = False))
+
+                natName = jsonData['response']['body']['items']['item']['natKorNm']
+                natName = natName.replace(' ', '')
+                num = jsonData['response']['body']['items']['item']['num']
+                ed = jsonData['response']['body']['items']['item']['ed']
+                print('[ %s_%s : %s ]' % (natName, yyyymm, num))
+                print('------------------------------------------------------')
+                jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
+                result.append([natName, nat_cd, yyyymm, num])
+
+    return (jsonResult, result, natName, ed)
