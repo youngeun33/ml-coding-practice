48f872215e01b777f21e605938ce95a5e40f9e8d
김영은 <yejy020306@gmail.com>
2026-04-15 09:38:39 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -18,12 +18,12 @@ def main():
   nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
   ed_cd = "E" 		                      # E : 방한외래관광객, D : 해외 출국
 
-  jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
+  jsonResult, result, natName, dataEND = getTourismStatsService(NAT_CD, ED_CD, nStartYear, nEndYear)  #[CODE 3]
 
   #파일저장 : csv 파일
   columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
   result_df = pd.DataFrame(result, columns = columns)
-  result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
+  result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ED_CD, nStartYear, dataEND), index = False, encoding = 'cp949')
 
 """### [CODE 3]"""
 
