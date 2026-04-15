05a9827082c534d87d7056141cc9a5f539751c08
김영은 <yejy020306@gmail.com>
2026-04-15 09:27:54 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,7 +4,7 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = "자신의 Service Key"
+ServiceKey = "200281100206dd4ac6cdacf7d0e3a9061cf1502ccd847851812036590082c162"
 
 """### [CODE 0]"""
 
