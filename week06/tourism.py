72f1647cc5d307509418429d865edda773876910
김영은 <yejy020306@gmail.com>
2026-04-15 09:24:25 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -78,5 +78,13 @@ def getTourismStatsItem(yyyymm, not_cd, ed_cd):
 def getRequestUrl(url):  #[CODE 1]
     req = urllib.request.Request(url)
     try:
-    response = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+        return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
 
+mai()
