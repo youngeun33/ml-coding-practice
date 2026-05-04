76f7339ad4a4ad5791766306fc9899bad4d4977c
김영은 <yejy020306@gmail.com>
2026-05-04 14:45:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -3,5 +3,8 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', as_frame=False)
 
-print(mnist
-      )
+print(mnist.keys())  # data와 target만 사용
+
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape) 
