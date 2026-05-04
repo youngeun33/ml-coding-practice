d91979aae6ec852edcad5ac49657d3c5090a220a
김영은 <yejy020306@gmail.com>
2026-05-04 15:17:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -84,8 +84,11 @@ from sklearn.metrics import f1_score
 print(f1_score(y_train_5, y_train_pred))
 
 # ROC 곡선
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
 
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5,cv=3,
                              method="decision_function")
 roc_auc_score(y_train_5, y_scores)
+
+#다음은 실행하는데 몇 분이 걸릴 수 있음
+from sklearn.metrics import Ran
