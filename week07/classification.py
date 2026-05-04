5f7638a9ceb55ad70608bcf00f2cb75891dfbefe
김영은 <yejy020306@gmail.com>
2026-05-04 14:50:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -32,3 +32,5 @@ for idx, image_data in enumerate(X[:100]):
 plt.subplots_adjust(wspace=0, hspace=0)
 plt.show()
 
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
