6ac481bf47310cb4770f7e43873a165723f9b1e8
김영은 <yejy020306@gmail.com>
2026-05-04 14:51:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -39,4 +39,7 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
-from sklearn
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_t)
