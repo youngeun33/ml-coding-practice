71375b20e431796f294794ab6be4079d02b74736
김영은 <yejy020306@gmail.com>
2026-05-04 14:56:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -62,6 +62,10 @@ print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
 # 오차 행렬
 from sklearn.model_selection import cross_val_predict
 
-y_train_pred = corss_val_predict(sgd_clf, X_train, y_train_5, cv=3) 
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3) 
 
-from sklearn.metrics import conf
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
+
