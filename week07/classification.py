067d667d6a4c28559a4966cd7e5a90c83cd10c78
김영은 <yejy020306@gmail.com>
2026-05-04 15:19:22 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -98,4 +98,5 @@ forest_clf = RandomForestClassifier(random_state=42)
 y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
                                    method="predict_proba")
 
-y_scores_forest = y_probas_forest
+y_scores_forest = y_probas_forest[:,1]
+y_train_pred_forest = y_probas_forest[:,1]
