cfe5c16a04bb1aa1ac6a42d07fd667f88aa8b4da
김영은 <yejy020306@gmail.com>
2026-05-04 14:46:22 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -7,4 +7,8 @@ print(mnist.keys())  # data와 target만 사용
 
 X, y = mnist.data, mnist.target
 print(X)
-print(X.shape) 
+print(X.shape)      # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
+
+import mat
