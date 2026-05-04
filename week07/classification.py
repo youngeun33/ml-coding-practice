94dd73ccf767d131b8588686db49d81151287f96
김영은 <yejy020306@gmail.com>
2026-05-04 14:52:22 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -42,4 +42,8 @@ y_test_5 = (y_test == '5')
 from sklearn.linear_model import SGDClassifier
 
 sgd_clf = SGDClassifier(random_state=42)
-sgd_clf.fit(X_t)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
+# 성능 측정 - 교차 검증을 사용한 정확
