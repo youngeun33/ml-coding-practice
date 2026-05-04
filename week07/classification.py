bb6699bd712b41724dbda8bd85a05878f45c8d74
김영은 <yejy020306@gmail.com>
2026-05-04 15:19:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -99,4 +99,9 @@ y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
                                    method="predict_proba")
 
 y_scores_forest = y_probas_forest[:,1]
-y_train_pred_forest = y_probas_forest[:,1]
+y_train_pred_forest = y_probas_forest[:,1] >= 0.5 # 양성 확률 ≥ 50%
+
+print(f1_score(y_train_5, y_train_pred_forest))
+print(roc_auc_score(y_train_5, y_scores_forest))
+print(precision_score(y_train_5, y_train_pred_forest))
+print(recall_score(y_train_5, y_train_pred_forest))
