9f2184d28883039f1032b159cdb72b9cada87160
김영은 <yejy020306@gmail.com>
2026-05-04 14:55:22 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -57,4 +57,4 @@ dummy_clf = DummyClassifier()
 dummy_clf.fit(X_train, y_train_5)
 print(any(dummy_clf.predict(X_train)))
 
-print(cross_val)
+print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
