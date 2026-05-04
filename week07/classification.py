666cdae652020137ba69b632f996f512e616f656
김영은 <yejy020306@gmail.com>
2026-05-04 14:58:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -77,4 +77,6 @@ from sklearn.metrics import precision_score, recall_score
 
 print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
 
-print(recall_score(y_train_5, y_train_pred))     # == 3530 / (3530 + 330)
+print(recall_score(y_train_5, y_train_pred))     # == 3530 / (1891 + 3530)
+
+from sk
