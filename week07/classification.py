379892236b2a1cd9e1a364f4f1d3622fb1feb9de
김영은 <yejy020306@gmail.com>
2026-05-04 14:57:22 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -69,3 +69,5 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_train_5, y_train_pred)
 print(cm)
 
+y_train_perfect_predictions = y_train_5 # 완벽한 분류기일 경우
+cm_perfect = confusion_matrix(y_train
