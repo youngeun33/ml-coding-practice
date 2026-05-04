970d86d197555a7b44fc1d8c6d2a6774b2cf0e81
김영은 <yejy020306@gmail.com>
2026-05-04 14:49:52 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -24,4 +24,8 @@ plt.show()
 
 print(y[0])        # 샘플 데이터 레이블 확인 (=5)
 
-# 10x10 rmfal to
+# 10x10 그림 생성
+plt.figure(figsize=(9,9))
+for idx, image_data in enumerate(X[:100]):
+    plt.subplot(10, 10, idx + 1)
+    plot_digit(image_data)
